1fc1e5ae014f2c28a6d87bcf8f6cc4fd86988639
suyash musale <musalesuyash97@gmail.com>
2025-03-20 12:26:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import '@mediapipe/tasks-vision';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF, useAnimations } from '@react-three/drei';
+import { Suspense } from 'react';
+
+function Soldier({ animationName, position }) {
+    const { scene, animations } = useGLTF('/Soldier.glb');
+    const { actions } = useAnimations(animations, scene);
+
+    actions[animationName]?.play();
+
+    return <primitive object={scene} position={position} scale={[1.5, 1.5, 1.5]} />;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Canvas camera={{ position: [0, 3, 10], fov: 50 }}>
+            <ambientLight intensity={0.8} />
+            <directionalLight position={[5, 5, 5]} intensity={1} />
+
+            <Suspense fallback={null}>
+                <Soldier animationName="Idle" position={[-3, 0, 0]} />
+                <Soldier animationName="Walk" position={[0, 0, 0]} />
+                <Soldier animationName="Run" position={[3, 0, 0]} />
+            </Suspense>
+
+            <mesh position={[0, -2.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+                <planeGeometry args={[20, 20]} />
+                <meshStandardMaterial color="gray" />
+            </mesh>
+
+            <OrbitControls />
+        </Canvas>
+    );
 }
 
 export default App;
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ignoreWarnings: [
+        {
+            module: /@mediapipe\/tasks-vision/,
+            message: /Failed to parse source map/
+        }
+    ]
+};
